8029789ec8d626bcbb5d83f336eaeb33b1557748
Jonathan Rothberg <jrothberg@jrothbergmacbookpro.local>
2017-02-24 17:14:28 -0800

    Typer work

--- rusty_lamp_lib/src/generator/typer.rs
+++ rusty_lamp_lib/src/generator/typer.rs
@@ -4,11 +4,79 @@
  */
 
 use std::collections::{ HashMap, HashSet };
+use std::fmt;
 use parser::ast::{Statement, StatementKind, DataTypeStatement};
 use generator::resolver::ResolveError;
 
+pub struct TypeDefinition {
+    identifier: String,
+    num_params: i8,
+    
+}
+
+#[derive(PartialEq, Eq, Clone, Debug, Hash)]
+pub enum TypeDefinitionKind {
+    Primitive(String, String, String, String, String, String, String, String),
+    String,
+    Binary,
+    Optional,
+    Date,
+    List,
+    Set,
+    Map,
+    UserObject(String)
+}
+
+impl fmt::Display for TypeDefinitionKind {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let printable = match *self {
+            TypeDefinitionKind::Primitive(ref n, ..) => {
+                format!("{}", n)
+            },
+            TypeDefinitionKind::String => {
+                "string".into()
+            },
+            TypeDefinitionKind::Binary => {
+                "binary".into()
+            },
+            TypeDefinitionKind::Optional => {
+                "optional".into()
+            },
+            TypeDefinitionKind::Date => {
+                "date".into()
+            },
+            TypeDefinitionKind::List => {
+                "list".into()
+            },
+            TypeDefinitionKind::Set => {
+                "set".into()
+            },
+            TypeDefinitionKind::Map => {
+                "map".into()
+            },
+            TypeDefinitionKind::UserObject(ref n) => {
+                format!("{}", n)
+            }
+        };
+
+        write!(f, "{}", printable)
+    }
+
+    
+}
+
+impl TypeDefinitionKind {
+    fn num_params(&self) -> i8 {
+        match *self {
+            TypeDefinitionKind::Map => 2,
+            TypeDefinitionKind::List | TypeDefinitionKind::Set | TypeDefinitionKind::Optional => 1,
+            _ => 0
+        }
+    }
+}
+
 pub struct Typer {
-    table: HashMap<String, Statement>,
+    table: HashMap<String, TypeDefinitionKind>,
 }
 
 impl Typer {
@@ -23,10 +91,11 @@ impl Typer {
     }
 
     fn populate_builtin_types(&mut self) {
+        // self.table.insert("i8", TypeDefinitionKind::Primitive("i8".into(), ));
     }
 
-    pub fn insert_type(&mut self, key: &String, stmt: &Statement) -> Result<(), ResolveError> {
-        self.table.insert(key.clone(), stmt.clone());
+    pub fn insert_type(&mut self, key: &String, td: &TypeDefinitionKind) -> Result<(), ResolveError> {
+        self.table.insert(key.clone(), td.clone());
         return Ok(());
     }
 
